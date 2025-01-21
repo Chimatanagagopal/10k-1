d9e88c6fca229dc2b2863e89a00cf74258c64376
chimatanagagopal <chimata.nagagopal04@gmail.com>
2025-01-21 11:26:09 +0530

    arthamatic oprations

---
+++ python/1.basics.py
@@ -0,0 +1,31 @@
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+
+
+a=2  #a,b are variables
+b=3
+#Arithmetic oprations
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+a=10
+print(type(a)) #type 
+print(a+b)
+print(a-b)
+
+#sring
+_yuvaraj="what is your name"
+print(_yuvaraj)
+print(_yuvaraj[1])
+print(_yuvaraj[0])   #string indexing
+print(_yuvaraj[-1])
+
+print(_yuvaraj[0:4])
+print(_yuvaraj[-4:-1])  #sting slicing
+
+print(len(_yuvaraj)) #length function
