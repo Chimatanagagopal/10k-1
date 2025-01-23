d342af647a8d119c46280986c317c3e5d1767491
chimatanagagopal <chimata.nagagopal04@gmail.com>
2025-01-23 22:40:38 +0530

    css exam

---
+++ python/sequence.py
@@ -0,0 +1,26 @@
+list=['chimata','naga','gopal',2,5,['simple','comp'],True]
+print(len(list))
+print(list[2])
+print(list[5][-1])
+print(len(list[5]))
+k=list[5]
+print(k)
+print(k[0])
+print(list[1:])
+print(list[:1])
+print(list[-1:])
+list[6]=False
+#list[7]=True
+print(list)
+print(list[1: :-1])
+print(len(list[5][1]))
+
+
+#tuple
+t=(2,2,5,6,7)
+print(type(t))
+print(t)
+#t[-1]='gopal'
+#print(t)
+print(t.index(2))
+
